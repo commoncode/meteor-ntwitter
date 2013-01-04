689e1831037dc4f5884cdc67374f3642b31cf58b
Daryl Antony <daryl@commoncode.com.au>
2013-01-04 18:13:46 +1100

    provided abstract methods for calling serverside

--- ntwitter.js
+++ ntwitter.js
@@ -1,1 +1,34 @@
-nTwitter = __meteor_bootstrap__.require('ntwitter')
+Meteor.nTwitter = __meteor_bootstrap__.require('ntwitter')
+
+Meteor.nTwitter.prototype.streamTweets = function(keywords, product_slug) {
+    console.log('... ... streamTweets')
+    this.stream(
+        'statuses/filter',
+        { track: keywords },
+        function(stream) {
+            stream.on('data', function(tweet) {
+            console.log(tweet.text);
+              Fiber(function() {
+                  tweet.product_slug = product_slug
+                  id = Tweets.insert(tweet)
+              }).run();
+            });
+            stream.on('error', function(error, code) {
+                console.log("My error: " + error + ": " + code);
+            });
+            stream.on('end', function (response) {
+              // Handle a disconnection
+              console.log("Twitter connection ended: " + response)
+            });
+            stream.on('destroy', function (response) {
+              // Handle a 'silent' disconnection from Twitter, no end/error event fired
+              console.log("Twitter destroyed the connection: " + response)
+            });
+        }
+    );
+}
+
+Meteor.nTwitter.prototype.destroyTweets = function() {
+    console.log('... ... destroyTweets')
+    this.stream.destroy
+}
