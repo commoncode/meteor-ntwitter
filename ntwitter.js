5164a9ff7e12d4e0100a83bc6c6727a833a52521
Daryl Antony <daryl@commoncode.com.au>
2013-01-07 13:51:04 +1100

    abstractions // added wrapper around search API

--- ntwitter.js
+++ ntwitter.js
@@ -1,22 +1,23 @@
 Meteor.nTwitter = __meteor_bootstrap__.require('ntwitter')
 
-Meteor.nTwitter.prototype.streamTweets = function(keywords, extra_data) {
+Meteor.nTwitter.prototype.streamTweets = function(keywords, collection_str, options) {
     console.log('... ... streamTweets')
-    this.stream(
+    var _this = this
+    _this.stream(
         'statuses/filter',
         { track: keywords },
         function(stream) {
             stream.on('data', function(tweet) {
               console.log(tweet.text);
               Fiber(function() {
-                // Store some extra data on this tweet
-                _.extend(tweet, extra_data)
+                // Store some options on this tweet to help filtering out later
+                _.extend(tweet, options)
                 // Insert the Tweet into the Collection
-                id = Tweets.insert(tweet)
+                id = eval(collection_str).insert(tweet)
               }).run();
             });
             stream.on('error', function(error, code) {
-                console.log("My error: " + error + ": " + code);
+                console.log("My error: " + error + ": " + code)
             });
             stream.on('end', function (response) {
               // Handle a disconnection
@@ -26,11 +27,26 @@ Meteor.nTwitter.prototype.streamTweets = function(keywords, extra_data) {
               // Handle a 'silent' disconnection from Twitter, no end/error event fired
               console.log("Twitter destroyed the connection: " + response)
             });
+            _this.current_stream = stream
         }
     );
 }
 
 Meteor.nTwitter.prototype.destroyTweets = function() {
     console.log('... ... destroyTweets')
-    this.stream.destroy
+    var _this = this
+    console.log('... ... ... attempting to destroy the current stream: ' + _this.current_stream.destroy)
+}
+
+Meteor.nTwitter.prototype.searchTweets = function(query, collection_str) {
+  console.log('... ... searchTweets: ' + query)
+  var _this = this
+
+  _this.search(query, {}, function(err, data) {
+    // console.log(data)
+    _collection = eval(collection_str)
+    Fiber(function() {
+      _collection.insert(data)
+    }).run()
+  });
 }
